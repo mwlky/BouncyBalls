2fcf44606ae039a85f696ab60f8d269a5dac1626
mwlky <mwlky@getgoogleoff.me>
2024-05-30 18:57:06 +0200

    Added FPS counter

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -1,68 +1,77 @@
 #include "Core.h"
 #include <SFML/Graphics/Font.hpp>
-#include <SFML/Graphics/Text.hpp>
 #include <SFML/System/Clock.hpp>
-#include <SFML/System/Vector2.hpp>
 #include <iostream>
 
 namespace Core {
 
-Core::Core() {
+    Core::Core() {
 
-  m_Window = new sf::RenderWindow(sf::RenderWindow(
-      sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Bouncy Balls"));
+        m_Window = new sf::RenderWindow(sf::RenderWindow(
+                sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Bouncy Balls"));
 
-  m_Window->setFramerateLimit(60);
+        m_Window->setFramerateLimit(60);
 
-  Init();
-}
-
-Core::~Core() { delete m_Window; }
-
-void Core::Init() {
-
-  sf::Clock deltaClock;
-  float lastTime = 0;
-
-  sf::Font font;
-  font.loadFromFile("../fonts/Coffee Fills.ttf");
-
-  while (m_Window->isOpen()) {
-
-    HandleEvents();
-    Render();
-    Tick();
-
-
-    float currentTime = deltaClock.restart().asSeconds();
-    float deltaTime = currentTime - lastTime;
-    float fps = 1.f / deltaTime;
-    lastTime = currentTime;
-
-    sf::Text Text("Fps " + std::to_string(fps), font);
-    m_Window->draw(Text);
-    std::cout << fps << std::endl;
-  }
-}
-
-void Core::HandleEvents() {
-
-  sf::Event outEvent;
-
-  while (m_Window->pollEvent(outEvent)) {
-    if (outEvent.type == sf::Event::Closed) {
-
-      m_Window->close();
+        Init();
     }
-  }
-}
 
-void Core::Render() {
+    Core::~Core() { delete m_Window; }
 
-  m_Window->display();
-  m_Window->clear();
-}
+    void Core::Init() {
 
-void Core::Tick() {}
+        sf::Clock deltaClock;
+        float lastTime = 0;
+
+        sf::Font font;
+        font.loadFromFile("../fonts/Coffee Fills.ttf");
+
+        while (m_Window->isOpen()) {
+
+            HandleEvents();
+            Render();
+
+            DisplayFPS(font, deltaClock);
+            float deltaTime = CalculateDeltaTime(lastTime, deltaClock);
+
+            Tick(deltaTime);
+        }
+    }
+
+    void Core::HandleEvents() {
+
+        sf::Event outEvent;
+
+        while (m_Window->pollEvent(outEvent)) {
+            if (outEvent.type == sf::Event::Closed) {
+
+                m_Window->close();
+            }
+        }
+    }
+
+    void Core::Render() {
+
+        m_Window->display();
+        m_Window->clear();
+    }
+
+    void Core::Tick(float deltaTime) {}
+
+    float Core::CalculateDeltaTime(float& lastTime, sf::Clock& clock) {
+
+        float currentTime = clock.restart().asSeconds();
+        float deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
+
+        return deltaTime;
+    }
+
+    void Core::DisplayFPS(const sf::Font &Font, sf::Clock &clock) {
+        float fps = 1.f / clock.getElapsedTime().asSeconds();
+        std::string text = "FPS: " + std::to_string(fps);
+
+        sf::Text textToDraw(text, Font);
+        m_Window->draw(textToDraw);
+    }
 
 } // namespace Core
--- src/core/Core.h
+++ src/core/Core.h
@@ -11,9 +11,12 @@ public:
 private:
 
     void Init();
-    void Tick();
+    void Tick(float deltaTime);
     void Render();
     void HandleEvents();
+
+    float CalculateDeltaTime(float& lastTime, sf::Clock& clock);
+    void DisplayFPS(const sf::Font& Font, sf::Clock& clock);
 
     const int SCREEN_WIDTH = 1280;
     const int SCREEN_HEIGHT = 720;
